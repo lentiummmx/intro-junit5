bddbc93e4a246a9a90136271a8acc8f5488f4e63
lentiummmx <lentium.mmx@gmail.com>
2021-04-05 22:21:40 -0500

    j-intro-junit5 File(s) committed to adding code improvements, lifecycle s4-39 (v0.1.0)

--- src/test/java/guru/springframework/GreetingTest.java
+++ src/test/java/guru/springframework/GreetingTest.java
@@ -1,13 +1,17 @@
 package guru.springframework;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class GreetingTest {
 
     private Greeting greeting;
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("Before - I'm only called once!!!");
+    }
 
     @BeforeEach
     void setUp() {
@@ -23,5 +27,15 @@ class GreetingTest {
     @Test
     void testHelloWorld() {
         System.out.println(greeting.helloWorld("Dan"));
+    }
+
+    @AfterEach
+    void tearDown() {
+        System.out.println("In AfterEach ...");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("After - I'm only called once!!!");
     }
 }
